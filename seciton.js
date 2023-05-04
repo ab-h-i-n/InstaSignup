a551abfb3cc79bd0eec7bb4fd60f133eb90a12d3
abhin_2122003 <abhinshelly@gmail.com>
2023-05-04 19:09:29 +0530

    done

--- seciton.js
+++ seciton.js
@@ -43,8 +43,5 @@ backbtn.addEventListener("click",()=>{
         i--;
         console.log('after ' +i);
     }
-    if(i===0){
-        window.location.replace("https://abhin-2122003.github.io/InstaLPage/#");
-    }
 
 });
